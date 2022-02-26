014ac60809bebc70b623fa63c8fff289e8c7fe86
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-02-26 22:08:54 +0530

    is subdtype

--- load_dataset.py
+++ load_dataset.py
@@ -7,6 +7,7 @@ from PIL import Image
 import imageio
 import os
 import numpy as np
+from numpy import issubdtype
 
 def iresize(arr, size, interp='bilinear', mode=None):
     im = Image.fromarray(arr, mode=mode)
